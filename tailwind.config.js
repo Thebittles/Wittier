9369f1b8361f8b17e754f27df2996394d608d1ee
Rikki Tomiko Ehrhart <122765747+rikkitomikoehrhart@users.noreply.github.com>
2025-04-26 14:12:33 -0500

    Add AustinTexas.gov brand colors and comparable font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'black1': '#1e1e1e',
+        'black2': '#282828',
+        'yellow': '#f6cb4a',
+        'blue': '#275edd',
+        'offwhite': '#f5f4f3',
+        'red': '#d74e3d',
+        'darkblue': '#22347e',
+      },
+      fontFamily: {
+        merriweather: ['Merriweather Sans', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 };
