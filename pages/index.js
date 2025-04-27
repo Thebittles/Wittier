6701ceaecbbc8b02a357a92b80af76cf44094415
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2025-04-27 11:04:41 -0500

    Fixed index links

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,22 @@
-
-import Layout from '../components/Layout';
-import dynamic from 'next/dynamic';
-import { useState, useEffect, useMemo } from 'react';
-import ResourceCard from '../components/ResourceCard';
+import Layout from "../components/Layout";
+import dynamic from "next/dynamic";
+import { useState, useEffect, useMemo } from "react";
+import ResourceCard from "../components/ResourceCard";
 
 export default function Home() {
   // Search Section
-  const [prompt, setPrompt] = useState('');
-  const [output, setOutput] = useState('');
+  const [prompt, setPrompt] = useState("");
+  const [output, setOutput] = useState("");
   const [loading, setLoading] = useState(false);
 
-
-  const Map = useMemo(() => dynamic(() => import('../components/MyMap'), {
-    loading: () => <p>Loading map...</p>,
-    ssr: false
-  }), []);
-
-
+  const Map = useMemo(
+    () =>
+      dynamic(() => import("../components/MyMap"), {
+        loading: () => <p>Loading map...</p>,
+        ssr: false,
+      }),
+    []
+  );
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -24,9 +24,11 @@ export default function Home() {
 
   return (
     <Layout>
-    <div className='container'>
+      <div className="container">
         <div className="min-h-screen flex flex-col items-center justify-center p-8">
-          <h1 className="text-4xl font-bold mb-8">Search Austin Community Resources</h1>
+          <h1 className="text-4xl font-bold mb-8">
+            Search Austin Community Resources
+          </h1>
           <div className="mt-8">
             {loading ? <p>Thinking...</p> : <p>{output}</p>}
           </div>
@@ -36,7 +38,7 @@ export default function Home() {
             // title="Mental Health Resources"
             description="Free and Low-cost Mental Health Programs"
             // description1="first aid, heimlich, CPR, bullet wound Treatment"
-            link="https://example.com/rent"
+            link="mentalhealth"
             image="MentalHealthResources.jpg"
           />
           <ResourceCard
@@ -88,16 +90,14 @@ export default function Home() {
       </div>
       <div className="callout">
         <div className="w-full inset">
-          <h1 className="text-4xl font-bold text-center mb-8">See Resources Near You!</h1>
+          <h1 className="text-4xl font-bold text-center mb-8">
+            See Resources Near You!
+          </h1>
           <div className="w-full map-container" style={{ height: "600px" }}>
             <Map />
           </div>
         </div>
       </div>
-
     </Layout>
   );
 }
-
-
-
